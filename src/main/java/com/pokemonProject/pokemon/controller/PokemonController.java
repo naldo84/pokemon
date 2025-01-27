6fa4d38aea5e255552d7f1fbb59d8786a17fb271
Erinaldo <erinaldo2005@gmail.com>
2025-01-27 19:47:30 -0300

    Inclusão da busca por id do BD e por numPokedex

--- src/main/java/com/pokemonProject/pokemon/controller/PokemonController.java
+++ src/main/java/com/pokemonProject/pokemon/controller/PokemonController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
@@ -26,5 +28,24 @@ public class PokemonController {
         System.out.println("PokemonModel" + pokemonModel);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(pokemonRepository.save(pokemonModel));
+    }
+
+    @GetMapping("/pokemons")
+    public ResponseEntity<List<PokemonModel>> consultarPokemons(){
+        return ResponseEntity.ok().body(pokemonRepository.findAll());
+    }
+
+    @GetMapping("/pokemon/{id}")   //Pesquisa pelo id do BD
+    public ResponseEntity<PokemonModel> consultarPokemonById(@PathVariable(value = "id")UUID id){
+        Optional<PokemonModel> pokemonEncontrado = pokemonRepository.findById(id);
+
+        return ResponseEntity.ok().body(pokemonEncontrado.get());
+    }
+
+    @GetMapping("/pokemons/numPokedex/{numPokedex}")
+    public ResponseEntity<PokemonModel> consultarByNumPokedex(@PathVariable(value = "numPokedex") int numPokedex){
+        Optional<PokemonModel> pokemonEncontrado = Optional.ofNullable(pokemonRepository.findByNumPokedex(numPokedex));
+
+        return ResponseEntity.ok().body(pokemonEncontrado.get());
     }
 }
